6db4009c5429844ea31956b1d9819c04fd0f3ebf
星星不见我 <164742031+hbhkl19@users.noreply.github.com>
2025-01-15 20:42:04 +0800

    Create alu_mux.v

---
+++ alu_mux.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+
+module alu_mux(
+input [31:0] immout,    //立即数的输出时alu的一个输入可能
+input [31:0] RD2,
+input ALUSrc,
+output [31:0] B
+    );
+assign B = (ALUSrc) ? immout : RD2;
+endmodule
